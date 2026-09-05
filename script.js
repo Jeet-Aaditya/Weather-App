ac2b2ba70a4c173063d40070a74af1d947f561bc
Jeet-Aaditya <aadijeet28@gmail.com>
2026-09-05 16:30:52 +0530

    Adding basic structure to get Weather

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
-const url = 'https://easy-weather1.p.rapidapi.com/daily/5?city=Lucknow';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '8483fb97fdmsh41fd5919b7588a7p181d36jsnacafb88a49db',
-		'x-rapidapi-host': 'easy-weather1.p.rapidapi.com',
-		'Content-Type': 'application/json'
-	}
-};
-async function getWeather() {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} 
+const temperature = document.getElementById("temp");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
+const condition = document.getElementById("condition");
+const weatherError = document.getElementById("weatherError");
+const cityName = document.getElementById("city");
 
-getWeather();
+function geoLocation(){
+	if(!navigator.geolocation){
+		fetchWeather("Tokyo");
+		return;
+	}
+
+	weatherError.textContent = "Locating…";
+	navigator.geolocation.getCurrentPosition(
+
+		function(position){
+			const lat = position.coords.latitude;
+			const lon = position.coords.longitude;
+			fetchWeatherByLocation(lat, lon);
+		},
+
+		function(){
+			fetchWeather("Tokyo");
+		}
+
+	);
+}
+
+
+async function fetchWeatherByLocation(lat, lon){
+	weatherError.textContent = "Loading...";
+
+	try{
+	const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=auto`;
+	const response = await fetch(url);
+	
+	const weather = await response.json();
+
+	    temperature.textContent =
+            Math.round(weather.current.temperature_2m) + "°C";
+
+        humidity.textContent =
+            weather.current.relative_humidity_2m;
+
+        wind.textContent =
+            Math.round(weather.current.wind_speed_10m);
+
+        condition.textContent =
+            getWeatherCondition(weather.current.weather_code);
+
+
+			 const locationResponse = await fetch(
+            `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`
+        );
+
+		const locationData = await locationResponse.json();
+
+		cityName.textContent = locationData.city || locationData.locality || "Unknown Location";
+		weatherError.textContent = "";
+
+	}
+		catch (error) {
+		weatherError.textContent = "Error fetching weather data.";
+		console.error(error);
+		}
+
+}
